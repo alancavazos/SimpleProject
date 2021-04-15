d92a6e479af9a4248b0605cf9eeab48811b04aa5
k3v1nn90 <77991437+k3v1nn90@users.noreply.github.com>
2021-04-15 15:47:00 -0500

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -8,29 +8,8 @@ import sys
 import argparse
 import subprocess
 
-# Grab the HOSTNAME and PORT to use for the HTTP connection from commandline arguments
-parser = argparse.ArgumentParser(description='Testing REST API.')
-parser.add_argument('--host', dest='HOSTNAME', default='DOCKER_HOST', help='Specify the hostname for the API (default: DOCKER_HOST)')
-parser.add_argument('--port', dest='PORT', default='5000', help='Specify port on the API host (default: 5000)')
-args = parser.parse_args()
-
 HOSTNAME = args.HOSTNAME
 PORT = args.PORT
-
-# Handle the special case of 'DOCKER_HOST'
-if HOSTNAME == 'DOCKER_HOST':
-    # group the docker host's IP address using the shell and `ip route`
-    HOSTNAME = subprocess.check_output(["/sbin/ip route | awk '/default/ { print $3 }'"], shell=True).strip()
-
-# Check that the host and port are valid; exit with error if they are not
-try:
-    r = requests.get('http://' + HOSTNAME + ':' + PORT + '/md5/test')
-except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.InvalidURL):
-    print "Unable to reach API at address: %s:%s...\n" % (HOSTNAME, PORT)
-    print "You can change the host or port using the --host and --port flags. Use -h for more info.\n"
-    sys.exit(1)
-else:
-    print "Testing REST API on %s:%s...\n" % (HOSTNAME, PORT)
 
 # Some constants for the API tests...
 HASH_1 = '098f6bcd4621d373cade4e832627b4f6'   # 'test'
@@ -45,28 +24,23 @@ tests = [
     ('/md5/hello%20world',        'GET',  [200], HASH_2),
     ('/md5',                      'GET',  [400,404,405], None),
     ('/factorial/4',              'GET',  [200], 24),
-    ('/factorial/5',              'GET',  [200], 120),
     ('/factorial/test',           'GET',  [400,404,405], None),
     ('/factorial/0',              'GET',  [200], 1),
     ('/fibonacci/8',              'GET',  [200], FIB_SEQ[:7]),
     ('/fibonacci/35',             'GET',  [200], FIB_SEQ),
     ('/fibonacci/test',           'GET',  [400,404,405], None),
-    ('/fibonacci/1',              'GET',  [200], FIB_SEQ[:3]),
     ('/is-prime/1',               'GET',  [200], False),
     ('/is-prime/2',               'GET',  [200], True),
-    ('/is-prime/5',               'GET',  [200], True),
-    ('/is-prime/6',               'GET',  [200], False),
-    ('/is-prime/37',              'GET',  [200], True),
     ('/slack-alert/test',         'GET',  [200], True),
     ('/slack-alert/'+HTTP_ENCODE, 'GET',  [200], True),
     ('/kv-retrieve/test1',        'GET',  [400,404,405], False),
-    ('/kv-record/test1',          'POST', [200], True, 'test1', 'foobar'),
-    ('/kv-retrieve/test1',        'GET',  [200], 'foobar'),
-    ('/kv-record/test1',          'POST', [400,404,405,409], False, 'test1', 'foobaz'),
-    ('/kv-record/test2',          'POST', [200], True, 'test2', '42'),
-    ('/kv-record/test1',          'PUT',  [200], True, 'test1', 'lorem ipsum'),
-    ('/kv-retrieve/test1',        'GET',  [200], 'lorem ipsum'),
-    ('/kv-record/test3',          'PUT',  [400,404,405,409], False, 'test3', '84')
+    ('/kv-record/test1',          'POST', [200], True, 'test1', 'fun'),
+    ('/kv-retrieve/test1',        'GET',  [200], 'fun'),
+    ('/kv-record/test1',          'POST', [400,404,405,409], False, 'test1', 'fun'),
+    ('/kv-record/test2',          'POST', [200], True, 'test2', '34'),
+    ('/kv-record/test1',          'PUT',  [200], True, 'test1', 'thing'),
+    ('/kv-retrieve/test1',        'GET',  [200], 'thing'),
+    ('/kv-record/test3',          'PUT',  [400,404,405,409], False, 'test3', '54')
 ]
 
 FAILED = 0
